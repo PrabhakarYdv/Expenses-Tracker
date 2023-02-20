38d17c79297b3ac7c78650a829017bf29692607a
Prabhakar <prabhakarkr802@gmail.com>
2023-02-20 23:02:55 +0530

    LOGIN & SIGNUP Validation

--- app/src/main/java/com/prabhakar/expensestracker/LoginActivity.kt
+++ app/src/main/java/com/prabhakar/expensestracker/LoginActivity.kt
@@ -1,16 +1,43 @@
 package com.prabhakar.expensestracker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         redirectRegister.setOnClickListener {
-            startActivity(Intent(this,SignupActivity::class.java))
+            startActivity(Intent(this, SignupActivity::class.java))
         }
+
+        btnLogin.setOnClickListener {
+            if (loginValidation()) {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        }
+    }
+
+    private fun loginValidation(): Boolean {
+        var isValidate = true
+        if (loginEmail.text == null) {
+            isValidate = false
+            loginEmail.error = "Please enter the email"
+        }
+        if (!loginEmail.text.toString().matches(emailPattern)) {
+            isValidate = false
+            loginEmail.error = "Please enter valid the email"
+        }
+        if (loginPassword.text.toString().isEmpty()) {
+            isValidate = false
+            loginPassword.error = "Please enter the password"
+
+        }
+
+        return isValidate
     }
 }
--- app/src/main/java/com/prabhakar/expensestracker/SignupActivity.kt
+++ app/src/main/java/com/prabhakar/expensestracker/SignupActivity.kt
@@ -1,16 +1,51 @@
 package com.prabhakar.expensestracker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
+    private val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
         redirectLogin.setOnClickListener {
-            startActivity(Intent(this,LoginActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
         }
+
+        btnRegister.setOnClickListener {
+            if (signUpValidation()) {
+                startActivity(Intent(this, MainActivity::class.java))
+
+            }
+        }
+    }
+
+    private fun signUpValidation(): Boolean {
+        var isValidate = true
+        if (registerName.text == null || registerName.text.isEmpty()) {
+            isValidate = false
+            registerName.error = "Please enter the name"
+        }
+        if (registerEmail.text == null) {
+            isValidate = false
+            registerEmail.error = "Please enter the email"
+        }
+        if (!registerEmail.text.toString().matches(emailPattern)) {
+            isValidate = false
+            registerEmail.error = "Please enter valid the email"
+        }
+        if (registerPassword.text.toString().length < 5) {
+            isValidate = false
+            registerPassword.error = "Please create a Strong Password"
+
+        }
+        if (registerConfirmPass.text.toString() != registerPassword.text.toString()) {
+            isValidate = false
+            registerConfirmPass.error = "Password and confirm password should be same"
+        }
+        return isValidate
     }
 }
