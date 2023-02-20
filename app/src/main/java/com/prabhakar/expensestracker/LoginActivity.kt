593b6f9d63a1e4bdf13e4093db5ce60977f5ac8e
Prabhakar <prabhakarkr802@gmail.com>
2023-02-20 21:58:12 +0530

    Connectivity of Activity Redirect

--- app/src/main/java/com/prabhakar/expensestracker/LoginActivity.kt
+++ app/src/main/java/com/prabhakar/expensestracker/LoginActivity.kt
@@ -1,11 +1,16 @@
 package com.prabhakar.expensestracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        redirectRegister.setOnClickListener {
+            startActivity(Intent(this,SignupActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/prabhakar/expensestracker/SignupActivity.kt
+++ app/src/main/java/com/prabhakar/expensestracker/SignupActivity.kt
@@ -1,11 +1,16 @@
 package com.prabhakar.expensestracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        redirectLogin.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
     }
 }
